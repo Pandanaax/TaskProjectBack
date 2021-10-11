1b2a7e7d2efd748196a84965a8d0568e331c4239
Randa <baliranda@gmail.com>
2021-10-11 16:14:38 +0200

    add api for delete all task

--- app/routes/task.routes.js
+++ app/routes/task.routes.js
@@ -5,5 +5,6 @@ const task = require("../controllers/task.controller.js");
 router.post("/create", task.created);
 router.get("/getAll", task.getAll);
 router.get("/finished", task.finished);
+router.post("/delete", task.delete);
 
 module.exports = router;
